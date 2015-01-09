a7fa2acbd49b58bfabd486afc741c5391de08feb
liesbethLewyllie <liesbeth.lewyllie@hogent.be>
2015-01-09 14:39:38 +0100

    extra constructor

--- src/domein/Klant.java
+++ src/domein/Klant.java
@@ -5,4 +5,14 @@ public class Klant {
 	private String naam;
 	private String voornaam;
 
+	/**
+	 * 
+	 * @param naam
+	 * @param voornaam
+	 */
+	public Klant(String naam, String voornaam) {
+		// TODO - implement Klant.Klant
+		throw new UnsupportedOperationException();
+	}
+
 }
