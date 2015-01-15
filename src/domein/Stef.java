a06e9480e5b92b173edfbcab0187cdc0df1946a5
Stefaan Samyn <stefaan.samyn@hogent.be>
2015-01-15 08:57:44 +0100

    Code nieuwe klasse en UML gewijzigd zelfde tijd, eerst commit code

---
+++ src/domein/Stef.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domein;
+
+/**
+ *
+ * @author Stefaan
+ */
+public class Stef {
+    private int justTesting;
+    private boolean doetHetHetGoed(){
+        return true;
+    }
+}
